8acce1a58eea295ebb94a5907dafb6bc331a87dd
Gaspacchio <gaspard.bariller@tutanota.com>
2020-06-10 14:08:07 +0200

    feat: add camera and lights

--- Game1.cs
+++ Game1.cs
@@ -14,7 +14,6 @@ namespace game
         protected override void Initialize()
         {
             base.Initialize();
-            DebugRenderEnabled = true;
 
             const int LIGHT_LAYER = 1;
             const int FOREGROUND_LAYER = 5;
@@ -22,8 +21,11 @@ namespace game
             const int OBJECT_LAYER = 7;
             const int BACKGROUND_LAYER = 8;
 
-            Scene.SetDefaultDesignResolution(1280, 720, Scene.SceneResolutionPolicy.ShowAllPixelPerfect);
             var scene = Scene.CreateWithDefaultRenderer(Color.LightGoldenrodYellow);
+            scene.SetDesignResolution(1280, 720, Scene.SceneResolutionPolicy.None);
+            scene.AddRenderer(new DeferredLightingRenderer(0, LIGHT_LAYER, FOREGROUND_LAYER, PLAYER_LAYER, OBJECT_LAYER, BACKGROUND_LAYER));
+
+            SetupLight(LIGHT_LAYER, scene);
 
             // Background image setup
             var landscape = scene.Content.LoadTexture("Scenes/background_dark_landscape");
@@ -48,6 +50,13 @@ namespace game
             hero.AddComponent(new Hero(PLAYER_LAYER));
             hero.SetPosition(new Vector2(Screen.Center.X, 600));
 
+            // Camera
+            var camera = new FollowCamera(hero, FollowCamera.CameraStyle.CameraWindow);
+            camera.FocusOffset = new Vector2(0, 225);
+            Debug.Log(camera.MapSize);
+            var cameraEntity = scene.CreateEntity("camera");
+            cameraEntity.AddComponent(camera);
+
             // Ground setup
             var ground = scene.CreateEntity("ground");
             ground.AddComponent(new BoxCollider(0, Screen.Height - 10, Screen.Width, 10));
@@ -57,17 +66,17 @@ namespace game
             MediaPlayer.Volume = 0.3f;
             MediaPlayer.Play(audioFile);
 
-            // Deffered lightning
-            // add the DeferredLightingRenderer to your Scene specifying which renderLayer contains your lights and an arbitrary number of renderLayers for it to render
-            var deferredRenderer = scene.AddRenderer(new DeferredLightingRenderer(0, LIGHT_LAYER, FOREGROUND_LAYER, PLAYER_LAYER, OBJECT_LAYER, BACKGROUND_LAYER));
-            var light = scene.CreateEntity("light");
-            light.AddComponent(new PointLight(Color.White)).SetRenderLayer(LIGHT_LAYER);
-            light.SetPosition(new Vector2(Screen.Center.X, 600));
-
-            // optionally set ambient lighting
-            // deferredRenderer.SetAmbientColor(Color.Blue);
-
             Scene = scene;
+        }
+
+        private static void SetupLight(int LIGHT_LAYER, Scene scene)
+        {
+            var light = new PointLight(Color.White);
+            light.SetIntensity(5f);
+            light.SetRadius(600f);
+            var lightEntity = scene.CreateEntity("light");
+            lightEntity.AddComponent(light).SetRenderLayer(LIGHT_LAYER);
+            lightEntity.SetPosition(new Vector2(Screen.Center.X, 600));
         }
     }
 }
