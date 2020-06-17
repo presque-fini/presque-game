65304dfd825019ccc2a4aaa92fe7ad64b16b6d3e
Gaspacchio <gaspard.bariller@tutanota.com>
2020-06-17 12:21:27 +0200

    refactor: type vars

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
 using Nez;
 using Nez.DeferredLighting;
@@ -21,48 +22,47 @@ namespace game
             const int OBJECT_LAYER = 7;
             const int BACKGROUND_LAYER = 8;
 
-            var scene = Scene.CreateWithDefaultRenderer(Color.LightGoldenrodYellow);
+            Scene scene = Scene.CreateWithDefaultRenderer(Color.LightGoldenrodYellow);
             scene.SetDesignResolution(1280, 720, Scene.SceneResolutionPolicy.None);
             scene.AddRenderer(new DeferredLightingRenderer(0, LIGHT_LAYER, FOREGROUND_LAYER, PLAYER_LAYER, OBJECT_LAYER, BACKGROUND_LAYER));
 
             SetupLight(LIGHT_LAYER, scene);
 
             // Background image setup
-            var landscape = scene.Content.LoadTexture("Scenes/background_dark_landscape");
-            var backgroundEntity = scene.CreateEntity("background");
+            Texture2D landscape = scene.Content.LoadTexture("Scenes/background_dark_landscape");
+            Entity backgroundEntity = scene.CreateEntity("background");
             backgroundEntity.AddComponent(new SpriteRenderer(landscape)).SetRenderLayer(BACKGROUND_LAYER);
             backgroundEntity.SetPosition(Screen.Center);
 
             // Foreground image setup
             SpriteAtlas atlas = scene.Content.LoadSpriteAtlas("Content/animations.atlas");
-            var foreground = scene.CreateEntity("rain");
-            var rain = foreground.AddComponent<SpriteAnimator>().AddAnimationsFromAtlas(atlas);
+            Entity foreground = scene.CreateEntity("rain");
+            SpriteAnimator rain = foreground.AddComponent<SpriteAnimator>().AddAnimationsFromAtlas(atlas);
             rain.RenderLayer = FOREGROUND_LAYER;
             foreground.SetPosition(Screen.Center);
             rain.Play("rain");
 
             //Radio setup
-            var radio = scene.CreateEntity("radio");
+            Entity radio = scene.CreateEntity("radio");
             radio.AddComponent(new Radio(BACKGROUND_LAYER));
 
             // Player setup
-            var hero = scene.CreateEntity("hero");
+            Entity hero = scene.CreateEntity("hero");
             hero.AddComponent(new Hero(PLAYER_LAYER, LIGHT_LAYER));
             hero.SetPosition(new Vector2(Screen.Center.X, 600));
 
             // Camera
-            var camera = new FollowCamera(hero, FollowCamera.CameraStyle.CameraWindow);
+            FollowCamera camera = new FollowCamera(hero, FollowCamera.CameraStyle.CameraWindow);
             camera.FocusOffset = new Vector2(0, 225);
-            Debug.Log(camera.MapSize);
-            var cameraEntity = scene.CreateEntity("camera");
+            Entity cameraEntity = scene.CreateEntity("camera");
             cameraEntity.AddComponent(camera);
 
             // Ground setup
-            var ground = scene.CreateEntity("ground");
+            Entity ground = scene.CreateEntity("ground");
             ground.AddComponent(new BoxCollider(0, Screen.Height - 10, Screen.Width, 10));
 
             // Audio setup
-            var audioFile = scene.Content.Load<Song>("Sound/ambient.wind-thunder-rain");
+            Song audioFile = scene.Content.Load<Song>("Sound/ambient.wind-thunder-rain");
             MediaPlayer.Volume = 0.3f;
             MediaPlayer.Play(audioFile);
 
@@ -71,10 +71,10 @@ namespace game
 
         private static void SetupLight(int LIGHT_LAYER, Scene scene)
         {
-            var light = new PointLight(Color.White);
+            PointLight light = new PointLight(Color.White);
             light.SetIntensity(2f);
             light.SetRadius(600f);
-            var lightEntity = scene.CreateEntity("light");
+            Entity lightEntity = scene.CreateEntity("light");
             lightEntity.AddComponent(light).SetRenderLayer(LIGHT_LAYER);
             lightEntity.SetPosition(new Vector2(Screen.Center.X, 250));
         }
