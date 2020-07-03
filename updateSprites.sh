a0402bb5356a43843ce35b8d0a7be7a4f81c71d3
Gaspacchio <gaspard.bariller@tutanota.com>
2020-07-03 16:35:30 +0200

    build: slightly improve updateSprites.sh

--- updateSprites.sh
+++ updateSprites.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+# Convert gif to png sequence using ffmpeg
+# ffmpeg -i ../art/assets/images/rain-transparent.gif  -vsync 0 ../art/sprites/rain/rain.00%d.png
 # Copy new image sequences from ../art
 cp -ruv ../art/sprites/. Content/Sprites/
 # Run spriteAtlasPacker
+echo "Running SpriteAtlasPacker"
 Content/SpriteAtlasPacker.exe -image:Content/animations.png -map:Content/animations.atlas -fps:24 Content/Sprites
 echo "You should rebuild animations.atlas and animations.png in the Pipeline Tool"
